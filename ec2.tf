820ff21876eba14610da212a4967438ef086d7bf
hyoeun <enqn07@daum.net>
2024-09-24 12:00:30 +0900

    ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -2,11 +2,49 @@ provider "aws" {
   region = "ap-northeast-2"
 }
 
-resource "aws_instance" "ec2" {
-  ami = "ami-0a5a6128e65676ebb"
-  instance_type = "t2.micro"
+# VPC 생성
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "10.0.0.0/16"
 }
 
+# 서브넷 생성
+resource "aws_subnet" "my_subnet" {
+  vpc_id     = aws_vpc.my_vpc.id
+  cidr_block = "10.0.1.0/24"
+}
+
+# EC2 인스턴스 생성
+resource "aws_instance" "ec2" {
+  ami           = "ami-0a5a6128e65676ebb"
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.my_subnet.id
+
+  # 보안 그룹 필요 시 추가
+  security_groups = [aws_security_group.my_sg.name]
+}
+
+# 보안 그룹 생성
+resource "aws_security_group" "my_sg" {
+  name        = "allow_ssh"
+  description = "Allow SSH inbound traffic"
+  vpc_id      = aws_vpc.my_vpc.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# EC2 인스턴스 퍼블릭 IP 출력
 output "ec2_ip" {
   value = aws_instance.ec2.public_ip
 }
